3d4b6f6f4cbdb4b4fcbc2f726fd66b21c2a68b9c
Grant Cloyd <dgcloyd@gmail.com>
2021-06-29 09:41:22 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+
+    def initialize(word)
+    @word = word
+end
+
+def match(query) 
+    query.select {|index| index.split('').sort == @word.split('').sort ? index : next } 
+   
+end
+
+end
